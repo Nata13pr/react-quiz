05b1a942d8bf33ccb33f851ebda049c924eedcbb
Nataliia <Nata13pr@gmail.com>
2023-08-15 13:30:34 +0300

    added 3 js files depended on status

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import { useEffect,useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
+import Loader from './Loader';
+import Error from './Error';
+import StartScreen from "./StartScreen";
+
 
 const initialState={
   questions:[],
@@ -26,7 +30,9 @@ function reducer(state,action){
 }
 
 export default function App() {
-const [state,dispatch]=useReducer(reducer,initialState)
+const [{questions,status},dispatch]=useReducer(reducer,initialState);
+
+const numQuestions=questions.length;
 
   useEffect(function(){
     fetch('http://localhost:9000/questions')
@@ -39,8 +45,11 @@ const [state,dispatch]=useReducer(reducer,initialState)
     <div className="app">
       <Header />
       <Main>
-        <p>1/15</p>
-        <p>Question?</p>
+        {status==='loading' && <Loader />}
+        {status==='error' && <Error/>}
+        {status==='ready' && <StartScreen 
+        numQuestions={numQuestions}/>}
+     
       </Main>
     </div>
   );
